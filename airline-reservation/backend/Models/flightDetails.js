09fb97a4f02396c9073b68c3919ac4e8abe4ff4d
aygupta9800 <ayushgupta9800@gmail.com>
2021-11-01 19:40:16 -0700

    flight table

---
+++ airline-reservation/backend/Models/flightDetails.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const flightDetailSchema = new Schema({
+    // _id: Number,
+    source: { type: String, required: true, trim: true },
+    destination: { type: String, required: true, trim: true },
+    arrivalDateTime: String,
+    deptartureDateTime: String,
+    duration: Number, // In minutes or hours
+    miles: Number, // distance in miles btw src and dest
+    basePrice: Number, // Minimum base price to book ticket in flight
+},
+{ 
+    versionKey: false 
+});
+
+const flightDetail = mongoose.model("FlightDetail", flightDetailSchema);
+
+export default flightDetail;
