a36c8f579e1dd634328aaddd084db0fcebca1582
aygupta9800 <ayushgupta9800@gmail.com>
2021-11-01 19:39:58 -0700

    user table model

---
+++ airline-reservation/backend/Models/users.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    // _id: Number,
+    userType: { type: String, required: true, trim: true },
+    // empId: String
+    firstName: { type: String, required: true, trim: true },
+    lastName: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true },
+    password: { type: String, required: true, trim: true },
+    phone: { type: String, trim: true },
+    token: {type: String, default: ''},
+    address: {
+        streetAddress: String,
+        aptNumber: String,
+        city: String,
+        state: String,
+        country: String,
+        zipcode: Number,
+    },
+    mileageNumber: {type: Number, default: 0},
+    gender: {type: String}
+},
+{ 
+    versionKey: false 
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
