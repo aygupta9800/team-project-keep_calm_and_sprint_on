13001753ab2fd4fd9d4afb1b4c47b511ccb1130c
aygupta9800 <ayushgupta9800@gmail.com>
2021-11-01 18:52:56 -0700

    Adding backend setup

---
+++ airline-reservation/backend/index.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import mongoose from "mongoose";
+import cors from 'cors';
+import connectMongoDB from './utils/dbConnection.js';
+
+// import signup from "./routes/signup.js";
+// import login from "./routes/login.js";
+// import restaurants from "./routes/restaurants.js";
+// import customers from "./routes/customers.js";
+// import logout from './routes/logout.js';
+// import imageUpload from './routes/imageUpload.js';
+// import passport from "passport";
+// import usepassport from './middleware/passport.js';
+
+const app = express();
+// const {mongoDB} = config
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+// app.use("/signup", signup);
+// app.use("/restaurants", restaurants);
+
+//Passport midlleware
+// app.use(passport.initialize());
+
+//passport config
+// usepassport(passport);
+
+// app.use("/login", login);
+// app.use("/customers", customers);
+// app.use("/logout", logout);
+// app.use("/imageUpload", imageUpload);
+
+const port = 3001;
+
+
+// Use of another path to see results for js_refresher.js
+app.get('/', (req, res) => {
+    res.send("Hello World")
+});
+
+connectMongoDB();
+
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to Database'));
+
+// To listen to port 3001
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+});
+
+export default app;
