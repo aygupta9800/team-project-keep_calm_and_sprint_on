a9963cc177aa75851c6ef64ace52c9ba2f1dd9e1
Ghanashri-m <ghanashri.28@gmail.com>
2021-11-02 19:19:40 -0700

    UI Landing page flow and routing set up

---
+++ airline-reservation/frontend/src/constants/index.js
@@ -0,0 +1,49 @@
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+
+export const ColorButton = withStyles(() => ({
+  root: {
+    color: '#222',
+    backgroundColor: '#87cefa',
+    '&:hover': {
+      backgroundColor: '#87cefa',
+      opacity: '0.9',
+    },
+  },
+}))(Button);
+
+export const ColorButton2 = withStyles(() => ({
+  root: {
+    color: '#222',
+    backgroundColor: '#C4AE78',
+    '&:hover': {
+      backgroundColor: '#C4AE78',
+      opacity: 0.6,
+    },
+  },
+}))(Button);
+
+export const ColorButton3 = withStyles(() => ({
+  root: {
+    color: 'white',
+    backgroundColor: '#222',
+    textTransform: 'none',
+    '&:hover': {
+      opacity: 0.6,
+    },
+  },
+}))(Button);
+
+export const ColorButton4 = withStyles(() => ({
+  root: {
+    color: '#222',
+    backgroundColor: '#87cefa',
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: '#87cefa',
+      opacity: 0.6,
+    },
+  },
+}))(Button);
+
