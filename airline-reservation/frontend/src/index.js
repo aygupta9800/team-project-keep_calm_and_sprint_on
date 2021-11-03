d403db40b74f8020204887a1b268deb19fd701ef
Ghanashri-m <ghanashri.28@gmail.com>
2021-11-02 19:20:21 -0700

    Code refactoring and routing set up

--- airline-reservation/frontend/src/index.js
+++ airline-reservation/frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from './state/store';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 ReactDOM.render(
   <Router>
