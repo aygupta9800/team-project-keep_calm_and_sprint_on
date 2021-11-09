38774d5d9dca86b802437c078539dce6e5b384fc
ViswaMithra16 <vmithravallabhaneni@gmail.com>
2021-11-09 15:22:18 -0800

    Added Login Page UI

--- airline-reservation/frontend/src/components/Login.js
+++ airline-reservation/frontend/src/components/Login.js
@@ -1,13 +1,43 @@
 import React from 'react';
 // import { useSelector } from 'react-redux';
 import LandingNavbar from './LandingNavbar/LandingNavbar.js';
+import { ColorButton2 } from '../constants/index';
+import '../components/styles.css';
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+
+
 
 const Login = () => {
+
+  const paperStyle={padding :20,height:'70vh',width:360, margin:"20px auto"}
+  
   return (
     <div>
       <LandingNavbar  />
-      <h1 style={{display: 'flex', alignItems: 'center', justifyContent: 'center',
-    height: '100vh'}}>This is Login component</h1>
+      <div className="landingpage">
+        
+      <Grid>
+                <Grid align='center'>
+                <Avatar src="/broken-image.jpg" />
+                    <h2 style={{fontSize:'35px'}}>Login</h2>
+                </Grid>
+                <TextField label='Username' variant="outlined" placeholder='Enter username' fullWidth required style={{margin:'20px auto', background:'white'}}/>
+                <TextField label='Password' variant="outlined" placeholder='Enter password' type='password' fullWidth required style={{margin:'20px auto', background:'white'}}/>
+                
+             <ColorButton2
+            variant='contained'
+            onClick={() => {
+            }}
+            style={{ height: '55px', alignSelf: 'center',margin:'20px auto'}}
+          >
+            Login
+          </ColorButton2>
+                
+            
+        </Grid>
+
+     
+          </div>
     </div>
   );
 };
