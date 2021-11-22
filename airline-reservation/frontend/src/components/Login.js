87c9188df924e3bfcc094b634ae870f5ef74c041
Ghanashri-m <ghanashri.28@gmail.com>
2021-11-21 19:07:36 -0800

    Code refactoring

--- airline-reservation/frontend/src/components/Login.js
+++ airline-reservation/frontend/src/components/Login.js
@@ -43,10 +43,10 @@ const Login = () => {
 
   const onLogIn = () => {
     if (userType === 'Employee') {
-      history.push('/EmployeeDashboard');
+      history.push({pathname: '/Airline', state: {userType: 'Employee'} });
       // dispatch(restaurantLogin({ userId, password }, history));
     } else {
-      history.push('CustomerDashboard');
+      history.push({pathname: '/Airline', state: {userType: 'Customer'} });
       // dispatch(customerLogin({ userId, password }, history));
     }
   };
