625e4414ec1d0812f9f970a8cc3a92a7766128d5
ViswaMithra16 <vmithravallabhaneni@gmail.com>
2021-11-28 19:29:26 -0800

    Added airline Profile Page

--- airline-reservation/frontend/src/state/action-creators/airlineActions.js
+++ airline-reservation/frontend/src/state/action-creators/airlineActions.js
@@ -1,38 +1,46 @@
 /* eslint-disable no-alert */
-import axios from 'axios';
-import server from '../../Config';
+import axios from "axios";
+import server from "../../Config";
 import {
   LOGOUT_SUCCESS,
   STORE_SEARCH_PARAMS,
   GET_AIRLINE_DETAILS,
-} from './types';
+} from "./types";
 
+const airlineDetails = null;
 
-export const storeSearchParams = (airlineDetails, history) => async (dispatch) => {
+export const storeSearchParams =
+  (airlineDetails, history) => async (dispatch) => {
     dispatch({
-        type: STORE_SEARCH_PARAMS,
-        payload: airlineDetails,
+      type: STORE_SEARCH_PARAMS,
+      payload: airlineDetails,
+    });
+  };
+
+export const getAirlineDetails = () => async (dispatch) => {
+  axios
+    .get(`${server}/airline`)
+    .then((response) => {
+      const airlineDetails = response.data;
+      console.log("airlineDetails = ", airlineDetails);
+      dispatch({
+        type: GET_AIRLINE_DETAILS,
+        payload: response.data,
+      });
+
+      return airlineDetails;
+    })
+    .catch((err) => {
+      alert(err);
+      return false;
     });
 };
 
-export const getAirlineDetails = () => async (dispatch) => {
-    axios.get(`${server}/airline`)
-      .then((response) => {
-        dispatch({
-          type: GET_AIRLINE_DETAILS,
-          payload: response.data,
-        });
-        return true;
-      })
-      .catch((err) => {
-        alert(err);
-        return false;
-      });
-  };
+export const logout = (history) => async (dispatch) => {
+  dispatch({
+    type: LOGOUT_SUCCESS,
+  });
+  history.push("/");
+};
 
-  export const logout = (history) => async (dispatch) => {
-    dispatch({
-      type: LOGOUT_SUCCESS,
-    });
-    history.push('/');
-  };
+export default airlineDetails;
