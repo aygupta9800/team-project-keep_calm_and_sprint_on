a83e258f7c8dcd8735956901fe81ae4feacfb832
aygupta9800 <ayushgupta9800@gmail.com>
2021-12-02 09:25:01 -0800

    changing backend url

--- airline-reservation/frontend/src/Config.js
+++ airline-reservation/frontend/src/Config.js
@@ -1,4 +1,4 @@
 // const server = 'http://localhost:3009';
-const server = 'http://cmpe202-airline-loadbalancer-1-1041265923.us-east-2.elb.amazonaws.com:3009';
-
+// const server = 'http://cmpe202-airline-loadbalancer-1-1041265923.us-east-2.elb.amazonaws.com:3009';
+const server = 'http://3.145.84.75:3009';
 export default server;
