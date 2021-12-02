b1f16b62e902f6c4df212af0434059112bed3525
aygupta9800 <ayushgupta9800@gmail.com>
2021-12-02 08:32:02 -0800

    changing cors url

--- airline-reservation/backend/index.js
+++ airline-reservation/backend/index.js
@@ -24,6 +24,20 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
+app.use(cors({ origin: 'http://3.16.41.65:3008', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:3008', credentials: true }));
+
+
+app.use(function (req, res, next) {
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3008');
+  res.setHeader('Access-Control-Allow-Origin', 'http://3.16.41.65:3008');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
 app.use(cors({ origin: 'http://localhost:3008', credentials: true }));
 
 app.use("/signup", signup);
