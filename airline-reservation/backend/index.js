72d14134b56d06153878fab9712ecd1acbeb630e
Ghanashri-m <ghanashri.28@gmail.com>
2021-12-02 16:57:50 -0800

    updating ip address

--- airline-reservation/backend/index.js
+++ airline-reservation/backend/index.js
@@ -24,13 +24,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
-// app.use(cors({ origin: 'http://3.145.84.75:3008', credentials: true }));
-app.use(cors({ origin: 'http://localhost:3008', credentials: true }));
+app.use(cors({ origin: 'http://3.145.84.75:3008', credentials: true }));
+// app.use(cors({ origin: 'http://localhost:3008', credentials: true }));
 
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3008');
-  // res.setHeader('Access-Control-Allow-Origin', 'http://3.145.84.75:3008');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3008');
+  res.setHeader('Access-Control-Allow-Origin', 'http://3.145.84.75:3008');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
